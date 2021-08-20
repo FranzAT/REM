22dcae56431746cea564a96e48f32067e86c6386
FranzAT <franz.heinzl@gmail.com>
2021-08-20 00:31:26 +0000

    set up database connection

--- main/settings/development.py
+++ main/settings/development.py
@@ -4,15 +4,20 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['franzat.eu.pythonanywhere.com']
+#ALLOWED_HOSTS = ['franzat.eu.pythonanywhere.com']
+ALLOWED_HOSTS = get_env_variable('SITE_HOST')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'FranzAT$default',
-        'USER': 'FranzAT',
+#        'NAME': 'FranzAT$default',
+        'NAME': get_env_variable('DATABASE_NAME'),
+#        'USER': 'FranzAT',
+        'USER': get_env_variable('DATABASE_USER'),
 #        'PASSWORD': 'password set in seperate file',
         'OPTIONS': {'read_default_file': '~/.my.cnf',},
-        'HOST': 'FranzAT.mysql.eu.pythonanywhere-services.com',
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+#        'HOST': 'FranzAT.mysql.eu.pythonanywhere-services.com',
+        'HOST': get_env_variable('DATABASE_HOST'),
     }
 }
